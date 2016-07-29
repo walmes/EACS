fd7a1cb3f125e60b3b5213b2bb086fcf309af0f0
Walmes Zeviani <walmes@ufpr.br>
2016-07-29 00:01:28 -0300

    Atualiza o EACS.R e o README.md.

--- R/EACS.R
+++ R/EACS.R
@@ -1,7 +1,35 @@
 #' @name EACS
 #' @title Estatística Aplicada à Ciência do Solo
 #' @docType package
-#' @description TODO
+#' @description Pacote que documenta conjuntos de dados de Projetos de
+#'     Pesquisa na Ciência do Solo. O pacote possui as análises dos
+#'     dados no formato de vinheta com o objetivo de disponilizar essas
+#'     análises como molde ou inspiração para problemas que sejam
+#'     semelhantes.
 #' @import wzRfun
+#' @examples
+#' \dontrun{
 #'
+#' # Carrega o pacote.
+#' library(EACS)
+#'
+#' # Lista os objetos do pacote.
+#' ls("package:EACS")
+#'
+#' # Abre a documentação do pacote.
+#' help(package = "EACS", help_type = "html")
+#'
+#' # Abre a lista de vinhetas no navegador.
+#' browseVignettes(package = "EACS")
+#'
+#' # Exibe a lista no console.
+#' vignette(package = "EACS")
+#'
+#' # Abre uma das vinhetas.
+#' vignette(topic = "v01_poisson", package = "EACS")
+#'
+#' # Citação do pacote.
+#' citation("EACS")
+#'
+#' }
 NULL
--- README.md
+++ README.md
@@ -6,3 +6,41 @@ Walmes Zeviani
 ## Organização
 
 ## Instalação
+
+Para instalar o pacote EACS, é necessário ter o pacote `devtools`. Com
+ele, execute o código abaixo em uma sessão R.
+
+```r
+# Carrega o devtools.
+library(devtools)
+
+# Instala do GitLab (sob desenvolvimento).
+install_git(url = "https://gitlab.c3sl.ufpr.br/walmes/EACS.git")
+
+# Instala do GitHub (estável)
+install_github(repo = "EACS", username = "walmes")
+```
+
+## Uso
+
+O código abaixo permite que fazer um *tour* pelo pacote EACS.
+
+```r
+# Carrega o pacote.
+library(EACS)
+
+# Lista os objetos do pacote.
+ls("package:EACS")
+
+# Abre a documentação do pacote.
+help(package = "EACS", help_type = "html")
+
+# Mostra as vinhetas disponíveis.
+browseVignettes(package = "EACS")
+
+# Abre alguma vinheta.
+vignette(topic = "vignette_name_here", package = "EACS")
+
+# Cita o pacote.
+citation("EACS")
+```
